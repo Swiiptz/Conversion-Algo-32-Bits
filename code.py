a53267c2787f98bc4b4fc909b1bba73561fcfda0
IG11exe <IG11exe@users.noreply.github.com>
2021-10-24 23:22:25 +0200

    - Changement de fonction (entier_base10 - rajout de float - ajout d'un "else"

--- code.py
+++ code.py
@@ -1,8 +1,10 @@
-x = input('Rentrer le nombre en base 10: ')
-def base10_2_entier(x):
-    if int(x)>0:
-      x = str(x)
-      y = x.find('.')
-      partie_décimal = x[0:y]
-    return partie_décimal
-print(base10_2_entier(x))
+x = input('Rentrer le nombre en base 2:')
+def entier_base10(x):  
+    if float(x) > 0:
+        a = str(x)
+        y = a.find('.')
+        partie_décimal = a[0:y]
+        return partie_décimal
+    else:
+        return "Le nombre est inférieur à 0"
+print(entier_base10(x))
