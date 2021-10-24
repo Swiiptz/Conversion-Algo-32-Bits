6db2c19e0dadd0cd3ee6dceb29ccd29580b4bcad
IG11exe <IG11exe@users.noreply.github.com>
2021-10-24 23:07:57 +0200

    Rajout du print()

--- code.py
+++ code.py
@@ -1,8 +1,8 @@
 x = input('Rentrer le nombre en base 10: ')
 def base10_2_entier(x):
-    if x>0:
+    if int(x)>0:
       x = str(x)
       y = x.find('.')
       partie_décimal = x[0:y]
     return partie_décimal
-print(base10_2_entier)
+print(base10_2_entier(x))
