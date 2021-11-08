ad814174da26acffba6e0d83fe71395fa422feaf
Swiiptz <90258450+Swiiptz@users.noreply.github.com>
2021-11-08 10:05:22 +0100

    Ajout de commentaires
    Ajout de commentaires au code.

--- code.py
+++ code.py
@@ -1,13 +1,13 @@
-x = input('Rentrer le nombre en base 10: ')
-def mantisse(x):
+x = input('Rentrer le nombre en base 10: ') #entrée
+def mantisse(x): #fonction mantisse
     
-    def entier_base10(x):  
+    def entier_base10(x):   #fonction qui extrait la partie entière
         a = str(x)
         y = a.find('.')
         partie_entière = a[0:y]
         return partie_entière
 
-    def decimal_base10(x):  
+    def decimal_base10(x):  #fonction qui extrait la partie décimal
         a = str(x)
         y = a.find('.')
         long = len(a)
@@ -15,10 +15,10 @@ def mantisse(x):
         partie_décimal = '0.' + str(partie_décimal)
         return partie_décimal #sortie str du décimal après la virugule de la forme 0.'decimal'
 
-    entier = int(entier_base10(x))
-    decimal = float(decimal_base10(x))
+    entier = int(entier_base10(x)) 
+    decimal = float(decimal_base10(x)) 
 
-    def base10_2_entier(entier):
+    def base10_2_entier(entier): #conversion de la partie entière en base 2
         a = bin(entier)       
         long = len(a)
         b = a[2:long]
@@ -26,46 +26,46 @@ def mantisse(x):
             b = ''
             return b
         else:
-            lenght = len(b)
+            lenght = len(b) #variable de la taille de la chaien
             return b
 
-    f_conv_entier = base10_2_entier(entier)
+    f_conv_entier = base10_2_entier(entier) #permet de savoir si la partie entièr est égal à 0
     if f_conv_entier == None:
-        lenght = 0
+        lenght = 0 
     else:
         lenght = len(f_conv_entier)
 
-    def base10_2_decimal(decimal):
+    def base10_2_decimal(decimal): ##conversion de la partie décimal en base 2
         b = float(decimal)
         l = 0
         y = str(0)
         z = str(1)
         a = 0
-        listeA = str()
-        long = 23 - lenght
-        while l < long:
+        listeA = str() 
+        long = 23 - lenght #taille de la chaine, soustraire la taille de la sortie totale de la mantisse
+        while l < long: #tant que la chaine est en dessous de la valeur max on continue
             b = b*2
             if b > 1:
-                listeA = listeA + z
+                listeA = listeA + z #permet de rajouter une valeur à la chaine "1"
                 b = b-1
-                l = l+1
+                l = l+1 #pour que la boucle while puisse etre validée
                 a = a+1
                 continue
             if b < 1:
-                listeA = listeA + y
-                l = l+1
-                a = a+1
+                listeA = listeA + y #permet de rajouter une valeur à la chaine "0"
+                l = l+1 #pour que la boucle while puisse etre validée
+                a = a+1 
                 continue
             if b==1:
-                listeA = listeA + z
-                l = long
+                listeA = listeA + z #permet de rajouter une valeur à la chaine "1"
+                l = long #pour sortir de la boucle while
                 a = a+1
                 break
         x = listeA
         return x #conversion partie décimal base 10 en base 2
 
 
-    addition_mantisse = str(base10_2_entier(entier)) + '.' + str(base10_2_decimal(decimal))
+    addition_mantisse = str(base10_2_entier(entier)) + '.' + str(base10_2_decimal(decimal)) #mise en forme de la mantisse
     return addition_mantisse
 mantisse = mantisse(x)
 c1 = mantisse[0]
@@ -73,7 +73,7 @@ if c1 == '.':
     c = 8 #0
 else:
     c = 7 #1
-def mantisse_vf(mantisse):
+def mantisse_vf(mantisse): #fonction mantisse version finale
  loca_point = mantisse.find('.') #localisation point dans la mantisse
  long_m = len(mantisse) #taille mantisse
  m_p1 = mantisse[0:loca_point]#partie gauche mantisse avant le point
@@ -135,9 +135,9 @@ def exposant_ent(mantisse):
     m_vf = m_c2
     return expo
 expos = exposant_ent(mantisse)
-def exposant(exposant_ent):
+def exposant(exposant_ent): #fonction exposant
  if c == 8:
-     expos = exposant_ent(mantisse)
+     expos = exposant_ent(mantisse) 
      expos = 127 - int(expos)
  if c == 7:
      expos = exposant_ent(mantisse)
